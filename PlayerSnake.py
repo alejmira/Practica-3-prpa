4f707747a4d225d39483c6f6cc54dede997fc5f1
hecrod03 <98985684+hecrod03@users.noreply.github.com>
2022-05-02 00:48:40 +0200

    Prueba de show_score

--- PlayerSnake.py
+++ PlayerSnake.py
@@ -14,6 +14,7 @@ from multiprocessing.connection import Client
 
 window_x= 720
 window_y = 480
+game_window = pygame.display.set_mode((window_x, window_y))
 
 black = pygame.Color(0, 0, 0)
 red = pygame.Color(255, 0, 0)
@@ -21,6 +22,7 @@ green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 yellow = pygame.Color(255, 255, 0)
 white = pygame.Color(255, 255, 255)
+score =[0,0]
 
 snake_speed = 15
 
@@ -71,8 +73,8 @@ class Game():
         self.players = [Snake("BLUE"), Snake("YELLOW")]
         self.apple = Apple()
         self.score = [0,0]
-        self.game_over = None
         self.running = True
+        self.game_window = pygame.display.set_mode((window_x, window_y))
     
     def get_player(self, number): # 0: BLUE, 1: YELLOW
         return self.players[number]
@@ -97,12 +99,6 @@ class Game():
     
     def set_score(self, score):
         self.score = score
-    
-    def get_game_over(self):
-        return self.game_over
-    
-    def set_game_over(self, i):
-        self.game_over = i
     
     def is_running(self):
         return self.running
@@ -132,61 +128,29 @@ class Game():
         self.set_apple_pos(gameinfo['pos_apple'])
         self.set_score(gameinfo['score'])
         self.running = gameinfo['is_running']
-        self.game_over = gameinfo['game_over']
     
-    def gameOver(self, i, game_window):
+    def show_score(self,game_window,score):
         game_window.fill(black)
-        my_font =  pygame.font.SysFont('times new roman', 50)
-        game_over_surface1 =my_font.render('Player 1 Score: ' + str(self.score[0]), True, blue)
-        game_over_rect1 = game_over_surface1.get_rect()
-        game_over_rect1.midtop = (window_x//2, window_y//6)
-        game_window.blit(game_over_surface1, game_over_rect1)
-        
-        game_over_surface2 = my_font.render('Player 2 Score: ' + str(self.score[1]), True, yellow)
-        game_over_rect2 = game_over_surface2.get_rect()
-        game_over_rect2.midtop = (window_x//2, window_y//3)
-        game_window.blit(game_over_surface2, game_over_rect2)
-        
-        if i == 1:
-            winner_surface = my_font.render("Player 1 WINNER!", True, green)
-            winner_rect = winner_surface.get_rect()
-            winner_rect.midtop = (window_x//2, window_y//2)
-            game_window.blit(winner_surface, winner_rect)
-            
-        elif i == 2:
-            winner_surface = my_font.render("Player 2 WINNER!", True, green)
-            winner_rect = winner_surface.get_rect()
-            winner_rect.midtop = (window_x//2, window_y//2)
-            game_window.blit(winner_surface, winner_rect)
-        
-        elif i == 3:
-            if self.score[0] > self.score[1]:
-                winner_surface = my_font.render("Player 1 WINNER!", True, green)
-                winner_rect = winner_surface.get_rect()
-                winner_rect.midtop = (window_x//2, window_y//2)
-                game_window.blit(winner_surface, winner_rect)
-            elif self.score[0] < self.score[1]:
-                winner_surface = my_font.render("Player 2 WINNER!", True, green)
-                winner_rect = winner_surface.get_rect()
-                winner_rect.midtop = (window_x//2, window_y//2)
-                game_window.blit(winner_surface, winner_rect)
-            elif self.score[0] == self.score[1]: # Sólo es empate si se chocan a la vez y tienen la misma puntuación
-                draw_surface = my_font.render("DRAW!", True, white)
-                draw_rect = draw_surface.get_rect()
-                draw_rect.midtop = (window_x//2, window_y//2)
-                game_window.blit(draw_surface, draw_rect)
-        
-        pygame.display.flip()
-        time.sleep(10)
-        pygame.quit()
-        quit()
-        
+        score_font = pygame.font.SysFont('arial', 26)
+    # Blue
+        score_surface1 = score_font.render('Player 1 score: ' + str(self.score[0]), True, blue)
+        score_rect1 = score_surface1.get_rect()
+        score_rect1.midtop = (70, 5)
+    
+    #Yellow
+        score_surface2 = score_font.render('Player 2 score: ' + str(self.score[1]), True, yellow)
+        score_rect2 = score_surface2.get_rect()
+        score_rect2.midtop = (window_x-90, 5)
+        return
+        self.game_window.blit(score_surface1, score_rect1)
+        self.game_window.blit(score_surface2, score_rect2)
+
+
 def main(ip_address):
     try:
         with Client((ip_address, 6111), authkey=b'secret password') as conn:
             pygame.init()
             pygame.display.set_caption("Snake")
-            game_window = pygame.display.set_mode((window_x, window_y))
             fps = pygame.time.Clock()
             
             
@@ -194,7 +158,7 @@ def main(ip_address):
             gameinfo = conn.recv()
             game.update(gameinfo)
             
-            #print(gameinfo)
+            print(gameinfo)
             
             while game.is_running():
                 
@@ -213,8 +177,10 @@ def main(ip_address):
                 conn.send("next")
                 gameinfo = conn.recv()
                 game.update(gameinfo) 
-                #print(gameinfo)
+                print(gameinfo)
                 
+                
+                game.show_score(game_window,score)
                 game_window.fill(black)
                 
                 for pos in game.players[0].body:
@@ -226,18 +192,7 @@ def main(ip_address):
                 
                 gameinfo = conn.recv()
                 game.update(gameinfo) 
-                #print(gameinfo)
-                
-                if game.game_over == 1:
-                    game.gameOver(1, game_window)
-                    game.stop()
-                elif game.game_over == 2:
-                    game.gameOver(2, game_window)
-                    game.stop()
-                elif game.game_over == 3:
-                    game.gameOver(3, game_window)
-                    game.stop()
-                
+                print(gameinfo)
                 pygame.display.update()
                 fps.tick(snake_speed)
                 
